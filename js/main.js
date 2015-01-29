d1a1e0e242cf926e46af5ac224cf9896b0e0ac0e
jpailthorpe <jordanpailthorpe@gmail.com>
2015-01-28 21:03:46 -0500

    first upload
    this is the completed version of the essay. Still has css issues with
    the header. has to do with padding

---
+++ js/main.js
@@ -0,0 +1,86 @@
+var scrollElement = null,
+	w = null,
+	resizeTimer = null,
+	mobileMinHeight = 520,
+	defaultMinHeight = 550,
+	minHeight = 550,
+	height = 600;
+
+// Main loop
+(function(){
+
+	scrollElement = $('html, body');
+	w = $(window);
+
+	scrollElement.each(function(i) {
+		var initScrollTop = $(this).attr('scrollTop');
+		$(this).attr('scrollTop', 0);
+	});
+
+	$('.downButton a').bind('click', function(event){
+		event.preventDefault();
+		var $this = $(this);
+		target = this.hash;
+		$target = $(target);
+		console.log($BODY);
+		scrollElement.stop().animate({
+			scrollTop: $target.offset().top
+		}, 500);
+	});
+
+	$('.upButton a').bind('click', function(event){
+		event.preventDefault();
+		scrollElement.stop().animate({
+			scrollTop: 0
+		}, 1000);
+	});
+
+	w.on('resize', function () {
+		if (resizeTimer) {
+			clearTimeout(resizeTimer);
+		}
+
+		resizeSections();
+		resizeTimer = setTimeout(function() {
+			var s = w.scrollTop(),
+				newPos = Math.round(s / height) * height;
+			resizeSections();
+			scrollElement.stop().animate({
+				scrollTop: newPos
+			}, 250);
+		}, 500);
+	});
+
+	resizeSections();
+
+	window.$main = {
+
+		init: function() {
+			_setupGlobals();
+			console.log('main ready');
+			return true;
+		}
+	};
+
+	function _setupGlobals() {
+		window.$BODY = $('body');
+	}
+
+})();
+
+// function resizeSections () {
+	// var h = w.height();
+	// if (w.width() < 600) { // Here's a hack that helps the content fit on mobile screens
+		// minHeight = mobileMinHeight;
+	// } else {
+		// minHeight = defaultMinHeight;
+	// }
+	// height = h < minHeight ? minHeight : h;
+	// $('.row').css('height', height);
+	
+	// if (minHeight == defaultMinHeight) {
+		// var offset = (height - minHeight) * 0.5;
+		// $('.wrapper').css('padding-top', offset);
+		// $('.wrapper-video').css('padding-top', offset * 0.5);
+	// } 
+// }
